5f883990c8f6bcf16751b89570382b1c6c1bf711
Travis <tzito93@gmail.com>
2022-03-30 12:23:01 -0500

    Board init method with pretty_print

---
+++ board.rb
@@ -0,0 +1,25 @@
+# a class representing an 8x8 chess board
+require 'colorize'
+
+class Board
+  attr_reader :rows
+
+  def initialize
+    @rows = Array.new(8) { Array.new(8, '  ') }
+  end
+
+  def pretty_print
+    rows.each_with_index do |row, row_idx|
+      row.each_with_index do |space, col_idx|
+        case row_idx.even?
+        when true
+          col_idx.even? ? color = :white : color = :black
+        else
+          col_idx.even? ? color = :black : color = :white
+        end
+        print space.colorize(:background => color)
+      end
+      puts
+    end 
+  end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'board'
+
+b = Board.new
+b.pretty_print
